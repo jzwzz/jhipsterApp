f2a2c2c70da2104daf148d5ab6c14d25cb3996ca
zengzhou.wang <zengzhou.wang@hpe-airchina.com>
2018-07-07 17:33:11 +0800

    generated source

---
+++ gallery/src/main/java/com/jzwzz/demo/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.jzwzz.demo.config.audit;
---
+++ gallery/src/main/java/com/jzwzz/demo/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.jzwzz.demo.web.rest.vm;
